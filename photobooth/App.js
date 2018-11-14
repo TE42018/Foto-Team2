598d288cf2447b15b38c23de4d0ea0ff3852f0e7
Alex <alexander.emland@elev.ga.ntig.se>
2018-11-14 10:21:20 +0100

    navigation structure

--- photobooth/App.js
+++ photobooth/App.js
@@ -1,33 +1,15 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import HomeScreen from './HomeScreen';
+import SettingsScreen from './SettingsScreen';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Settings: SettingsScreen,
+  },
+  {
+    initialRouteName: 'Home',
   }
-}
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default RootStack;
