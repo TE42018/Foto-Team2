d6e96ec57e6082f4d9f51d55b5de10f98143b7eb
VictorOlofsson <victor.olofsson@elev.ga.ntig.se>
2018-11-29 12:34:49 +0100

    dropdown seconds update

--- photobooth/feature-start-setting/StartSettingScreen.js
+++ photobooth/feature-start-setting/StartSettingScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button, Image, AppRegistry, ImageBackground, label } from 'react-native';
+import { View, Text, StyleSheet, Button, Image, AppRegistry, ImageBackground, Picker, Platform, Alert} from 'react-native';
 import SwitchSelector from 'react-native-switch-selector';
 import Icon from 'react-native-ionicons'
 import PhotoUpload from 'react-native-photo-upload'
@@ -8,10 +8,18 @@ export default class StartSettingScreen extends Component{
     static navigationOptions = {
         header: null,
     };
+    constructor(){
+        super();
+        this.state={
+          PickerSelectedVal : ''
+        }
+      }
+    getSelectedPickerValue=()=>{
+        Alert.alert("Selected country is : " +this.state.PickerSelectedVal);
+      }
     render() {
         return (
             <View style={styles.container}>
-
             <ImageBackground source={{uri: 'https://i2.wp.com/www.vgdl2.org/wp-content/uploads/2016/04/Dark-Grey-Background-1024x893.png?ssl=1'}} style={styles.backgroundImage} >
                 <View style={{flex: 1, flexDirection: "row"}}>       
                     <View style={{width: "50%", height: "100%", flex: 1}}>
@@ -20,17 +28,55 @@ export default class StartSettingScreen extends Component{
                             <View style={{paddingRight: "15%"}}>
                                 <Text style={{fontSize: 20, color: 'rgb(229, 229, 229)'}}>Camera</Text>
                             </View>
-                            <View style={{width: 150}}>
+                            <View style={{width: 200}}>
                                 <SwitchSelector fontSize={null} options={options} initial={0} onPress={value => console.log('all onPress with value: ${value}')} />
                             </View>
+                        </View>               
+
+                        <View style={{width: "50%", flexDirection: "row", alignItems: "center", padding: 30}}>
+                            <View style={{paddingRight: "15%"}}>
+                                <Text style={{fontSize: 20, color: 'rgb(229, 229, 229)'}}>Countdown seconds</Text>
+                            </View>
+                            <View style={{width: 200}}>
+                                <Picker
+                                    style={{color: "#fff"}}
+                                    mode="dropdown"
+                                    selectedValue={this.state.PickerSelectedVal}
+                                    onValueChange={(itemValue, itemIndex) => this.setState({PickerSelectedVal: itemValue})}>
+                                    <Picker.Item label="1 seconds" value="1 seconds" />
+                                    <Picker.Item label="2 seconds" value="2 seconds" />
+                                    <Picker.Item label="3 seconds" value="3 seconds" />
+                                    <Picker.Item label="4 seconds" value="4 seconds" />
+                                    <Picker.Item label="5 seconds" value="5 seconds" />
+                                    <Picker.Item label="6 seconds" value="6 seconds" />
+                                </Picker>
+                            </View>
                         </View>
-                        
-                        <Text style={{marginLeft: "8%", marginBottom: "10%", fontSize: 20, color: 'rgb(229, 229, 229)'}}>Countdown seconds</Text>
-                        <Text style={{marginLeft: "8%", marginBottom: "10%", fontSize: 20, color: 'rgb(229, 229, 229)'}}>start delay seconds</Text>                      
-                        <Text style={{marginLeft: "8%", fontSize: 20, color: 'rgb(229, 229, 229)', marginTop: "4%"}}><Icon ios="checkmark-circle" android="checkmark-circle" style={{color: "#fff", fontSize: 40}}/>  Show Get Ready Prompt</Text>
+
+                        <View style={{width: "50%", flexDirection: "row", alignItems: "center", padding: 30}}>
+                            <View style={{paddingRight: "15%"}}>
+                            <Text style={{fontSize: 20, color: 'rgb(229, 229, 229)'}}>Start delay seconds</Text>   
+                            </View>
+                            <View style={{width: 200}}>
+                                <Picker
+                                    style={{color: "#fff"}}
+                                    mode="dropdown"
+                                    selectedValue={this.state.PickerSelectedVal}
+                                    onValueChange={(itemValue, itemIndex) => this.setState({PickerSelectedVal: itemValue})} >
+                                    <Picker.Item label="1 seconds" value="1 seconds" />
+                                    <Picker.Item label="2 seconds" value="2 seconds" />
+                                    <Picker.Item label="3 seconds" value="3 seconds" />
+                                    <Picker.Item label="4 seconds" value="4 seconds" />
+                                    <Picker.Item label="5 seconds" value="5 seconds" />
+                                    <Picker.Item label="6 seconds" value="6 seconds" />
+                                </Picker>
+                            </View>
+                        </View>
+                   
+                        <Text style={{marginLeft: "8%", fontSize: 20, color: 'rgb(229, 229, 229)', marginTop: "14%"}}><Icon ios="checkmark-circle" android="checkmark-circle" style={{color: "#fff", fontSize: 40}}/>  Show Get Ready Prompt</Text>
                     </View>
                     <View style={{flex: 1, width: "50%", height: "50%"}}>
-                        <Text style={{marginTop: "38%", marginLeft: "8%", marginBottom: "10%", fontSize: 30, fontWeight: "bold", color: "white"}}>Background Image</Text>                      
+                        <Text style={{marginTop: "38%", marginLeft: "3%", marginBottom: "10%", fontSize: 30, fontWeight: "bold", color: "white"}}>Background Image</Text>                      
                         <View style={{flexDirection: "row", flex: 1}}>                         
                         <View>
                             <Text style={{marginLeft: "8%", marginBottom: "10%", fontSize: 20, color: 'rgb(229, 229, 229)'}}>Portrait </Text>
