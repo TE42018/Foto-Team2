0fe85b164f032efdeb2dcdfd2188d24c3d3ce139
JonatanW <jonatan.walett@elev.ga.ntig.se>
2018-12-07 10:07:36 +0100

    Added DesignSettingScreen

--- photobooth/App.js
+++ photobooth/App.js
@@ -3,12 +3,14 @@ import CamScreen from './Views/CamScreen';
 import StartSettingScreen from './Views/StartSettingScreen';
 import SettingsScreen from './Views/SettingsScreen';
 import EndScreen from './Views/EndScreen';
+import DesignSettingScreen from './Views/DesignSettingScreen';
 
 export default createStackNavigator(
   {
-    Start: StartSettingScreen,
-    Settings: SettingsScreen,
     Camera: CamScreen,
+    Start: StartSettingScreen,
+    Design: DesignSettingScreen,
+    Settings: SettingsScreen,
     End: EndScreen,
   },
 );
