b7ed20e90de85bd5d96ac4366ebe393d8a9774a5
crims <krille21@live.com>
2018-12-18 10:02:28 +0100

    fixad checkbox med alerts. i rätt position.

--- photobooth/SendOption.js
+++ photobooth/SendOption.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
 import {  View, Text, StyleSheet, Button, Image, AppRegistry, ImageBackground, Picker, Platform, Alert,CheckBox} from 'react-native';
-
+import PhoneInput from 'react-native-phone-input'
+import DialogInput from 'react-native-dialog-input';
 export default class SettingsScreen extends Component{
-    static navigationOptions = {
-        header: null,
-    };
+    constructor() {
+        super();
+    this.state={
+        check:false
+    }
+    this.state={
+        check2:false
+    }
+    }
+    checkBoxTest()
+    {
+        this.setState({
+            check: !this.state.check
+        })
+        alert("Now Value is " + !this.state.check)
+    }
+    checkBoxTest2()
+    {
+        this.setState({
+            check2: !this.state.check2
+        })
+        alert("Now Value is " + !this.state.check2)
+    }
     render() {
         return (
+            
             <View style={styles.container}>
                 <Text style={styles.align}>Send Options</Text>
                 <View>
-                    <CheckBox />
+                    <Text style={styles.alignSmallText}> Sms</Text>
+                    <CheckBox value={this.state.check} onChange={()=>this.checkBoxTest()}/>
+
+                    
+                </View>
+                <View style={{marginTop: "-11%"}}> 
+                <Text style={styles.alignSmall}> Email</Text>
+                    <CheckBox style={styles.checkBox}value={this.state.check2} onChange={()=>this.checkBoxTest2()}/>
                 </View>
             </View>
 
@@ -22,28 +51,32 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#464646',
+        padding: 90
     },
     align: {
         textAlign: 'left', 
         fontSize: 40, 
         color: "white", 
         fontWeight: "bold", 
-        marginLeft: "8%", 
-        marginTop: "8%",
+        marginBottom: "8%"
     },
     alignSmall: {
-        textAlign: 'left', 
-        fontSize: 25, 
         color: "white", 
         fontWeight: "bold", 
-        marginLeft: "8%", 
-        marginTop: "8%"
+        marginLeft: "80%", 
+        fontSize:20,
+        marginBottom: "2%"
+    },
+    alignSmallText: {
+        fontSize:20,
+        color:"white",
+        fontWeight: "bold", 
+        marginBottom: "2%"
     },
     checkBox: {
         textAlign: 'left', 
         color: "white", 
         fontWeight: "bold", 
-        marginLeft: "8%", 
-        marginTop: "8%"
+        marginLeft: "80%", 
     }
 });
